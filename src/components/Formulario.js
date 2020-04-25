be65a599f3366e76fe3f87010309371bcf3c6a6c
leydy <leydymayumy@gmail.com>
2020-04-25 10:31:19 -0500

    hook

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,8 +2,10 @@ import React, {Fragment, useState} from 'react';
 
 const Formulario = () => {
     const [datos, setDatos] = useState({
-        nombre: '',
-        apellido: ''
+        user: '',
+        email: '',
+        key: '',
+        phone: ''
     })
     const handleInputChange = (event) => {
         //console.log(event.target.value)
@@ -14,38 +16,85 @@ const Formulario = () => {
     }
     const enviarDatos = (event) => {
         event.preventDefault();
-        console.log(datos.nombre + ' ' + datos.apellido)
+        console.log(datos.user + ' ' + datos.email + ' ' + datos.key + ' ' + datos.phone)
     }
     return(
         <Fragment>
-            <h1>Formulario</h1>
-            <form className="row" onSubmit={enviarDatos}>
-                <div className="col-md-3">
-                    <input
-                         placeholder="Ingrese Nombre"
-                         className="form-control"
-                         type="text"
-                         name="nombre"
-                         onChange={handleInputChange}
-                    ></input>
+            <h6>Ingrese sus datos para el registro</h6>
+            <form className="inline" onSubmit={enviarDatos}>
+            <div className="col-sm-4 input-group">
+                <div className="input-group-prepend"></div>
+                <div className="input-group-text">
+                <i className="fa fa-user"></i>
+                
                 </div>
-                <div className="col-md-3">
-                    <input
-                        placeholder="Ingrese Apellido"
-                        className="form-control"
+                    <input 
+                        placeholder="Ingrese usuario"
                         type="text"
-                        name="apellido"
+                        name="user"
                         onChange={handleInputChange}
-                    ></input>
+                        className="form-control"></input>
                 </div>
-                <div className="col-md-3">
+                <div className="col-sm-4 input-group">
+                <div className="input-group-prepend"></div>
+                <div className="input-group-text">
+                <i className="fa fa-envelope"></i>
+                
+                </div>
+                    <input 
+                        placeholder="Ingrese correo"
+                        type="email"
+                        name="email"
+                        onChange={handleInputChange}
+                        className="form-control"></input>
+                </div>
+
+                <div className="col-sm-4 input-group">
+                <div className="input-group-prepend"></div>
+                <div className="input-group-text">
+                <i className="fa fa-key"></i>
+                
+                </div>
+                    <input 
+                        placeholder="Ingrese clave"
+                        type="password"
+                        name="key"
+                        onChange={handleInputChange}
+                        className="form-control"></input>
+                </div>
+
+                        
+                <div className="col-sm-4 input-group">
+                <div className="input-group-prepend">
+                <div className="input-group-text">
+                <i className="fa fa-phone"></i>
+                </div>
+                </div>
+                    <input 
+                        placeholder="Ingrese teléfono"
+                        type="number"
+                        name="phone"
+                        onChange={handleInputChange}
+                        className="form-control"></input>
+                </div>
+                <div className="col-sm-5">
                     <button className="btn btn-primary" type="submit">Enviar</button>
                     
                 </div>
+
+
+                
+
+                
                 
             </form>
+
+
+
+
             <h3>{datos.nombre}-{datos.apellido}</h3>
         </Fragment>
+        
         
     );
 }
