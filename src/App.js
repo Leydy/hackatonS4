9bd9c01dccbd7413fc023d10a335c4f3d4771416
leydy <leydymayumy@gmail.com>
2020-04-25 08:12:59 -0500

    pw

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <Formulario/>
+    </div>
+  );
+}
+
+export default App;
